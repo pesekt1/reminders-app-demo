d91470845bb61f3df7c4992331dbf872abd70623
Tomas Pesek <pesekt@gmail.com>
2022-05-30 20:26:42 +0200

    useState hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 import ReminderList from "./components/ReminderList";
 import Reminder from "./models/reminder";
 
-const reminders: Reminder[] = [
-  { id: 1, title: "Reminder 1" },
-  { id: 2, title: "Reminder 2" },
-];
+// const reminders: Reminder[] = [
+//   { id: 1, title: "Reminder 1" },
+//   { id: 2, title: "Reminder 2" },
+// ];
 
 function App() {
+  const [reminders, setReminders] = useState<Reminder[]>([
+    { id: 1, title: "Reminder 1" },
+  ]);
+
   return (
     <div className="App">
       <ReminderList items={reminders} />
